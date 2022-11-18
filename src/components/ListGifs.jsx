670c65c50f6c292411424dd291b7df37f065e01f
Alex Rubio <alejandro@flykube.com>
2022-11-18 15:38:17 +0100

    add some refactor

--- src/components/ListGifs.jsx
+++ src/components/ListGifs.jsx
@@ -1,39 +1,23 @@
+import ShareButton from './ShareButton';
 import styles from './ListGifs.module.css';
 
-const ListGifs = ({ listadeGifs }) => {
-	return (
-		<div className={styles.container}>
-			<div className={styles.container__list}>
-				{listadeGifs.map((item) => (
-					<div key={item.id} className={styles.container__gifs}>
-						<img src={item.url} alt={item.title} />
-						<p>{item.title}</p>
-						<button className={styles.dora} role="button"
-							onClick={() =>
-								fetch(item.url)
-									.then(function (response) {
-										return response.blob();
-									})
-									.then(function (blob) {
-										let file = new File([blob], 'picture.gif', { type: 'image/gif' });
-										let filesArray = [file];
+const ListGifs = ({ gifsList }) => {
+    const noResults = gifsList.length === 0;
 
-										if (navigator.canShare && navigator.canShare({ files: filesArray })) {
-											navigator.share({
-												files: filesArray,
-											});
-										}
-									})
-							}
-							type="button"
-						>
-							Compartir
-						</button>
-					</div>
-				))}
-			</div>
-		</div>
-	);
+    return (
+        <div className={styles.container}>
+            <div className={styles.container__list}>
+                {gifsList?.map((item) => (
+                    <div key={item.id} className={styles.container__gifs}>
+                        <img src={item.url} alt={item.title} />
+                        <p>{item.title}</p>
+                        <ShareButton itemUrl={item.url} />
+                    </div>
+                ))}
+                {noResults && <p className={styles.notFound}>No se ha encontrado ningún gif con esa búsqueda</p>}
+            </div>
+        </div>
+    );
 };
+
 export default ListGifs;
-// https://media4.giphy.com/media/TjAcxImn74uoDYVxFl/giphy-downsized-medium.gif?cid=e6f893cb4w867p2iyuiy36ej3rb1za22smcv8qqjpvpcxckf&rid=giphy-downsized-medium.gif&ct=g
--- src/page/App.jsx
+++ src/page/App.jsx
@@ -1,15 +1,25 @@
-import MostrarGifs from '../components/MostrarGifs';
+import { useState, useEffect, useRef } from 'react';
+import ListGifs from '../components/ListGifs';
+import Search from '../components//Search';
+import { fetchData } from '../api/fetchData';
+import './App.css';
 
 function App() {
-	return (
-		<>
-			<h1>GIFS ANIMADOS</h1>
-			<MostrarGifs keyword="Picachu" />;
-		</>
-	);
+    const [gifsList, setGifsList] = useState([]);
+
+    useEffect(() => {
+        fetchData().then((data) => {
+            setGifsList(data);
+        });
+    }, []);
+
+    return (
+        <>
+            <h1>GIFS ANIMADOS</h1>
+            <Search setGifsList={setGifsList} />
+            <ListGifs gifsList={gifsList} />
+        </>
+    );
 }
 
 export default App;
-// npm i gh-pages
-// npm run build
-// npm run deploy
